2a0f53b80041bf25c491af941f5873e1dc57f750
Joan Barnes <jbarnes@dbasystems.com>
2021-10-17 14:49:03 -0500

    basic context dictionary

--- projects/views.py
+++ projects/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
 
-def home(request):
-	return render(request, 'home.html', {})
+def home(request, year, month):
+	name = "Barnes"
+	return render(request,
+		'home.html', {
+		"name": name,
+		"year": year,
+		"month": month,
+		})
