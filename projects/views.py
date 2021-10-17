8d37ff52b1ed2e4aaa0eba10c754c14d3259a8b8
Joan Barnes <jbarnes@dbasystems.com>
2021-10-17 15:38:43 -0500

    2 calendar time works

--- projects/views.py
+++ projects/views.py
@@ -1,10 +1,28 @@
 from django.shortcuts import render
+import calendar
+from calendar import HTMLCalendar
+from datetime import datetime
 
-def home(request, year, month):
+def home(request, year=datetime.now().year, month=datetime.now().strftime('%B')):
 	name = "Barnes"
+	month = month.capitalize()
+
+# Convert month from name
+	month_number = list(calendar.month_name).index(month)
+	month_number = int(month_number)
+
+# Create a calendar
+	cal = HTMLCalendar().formatmonth(year, month_number)
+	now = datetime.now()
+
+	time = now.strftime('%I:%M %p')
+
 	return render(request,
 		'home.html', {
 		"name": name,
 		"year": year,
 		"month": month,
+		"month_number": month_number,
+		"cal" : cal, 
+		"time" : time,
 		})
